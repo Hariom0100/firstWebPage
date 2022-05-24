b6a0b99dbfe9f5b8541890c4d896de123ee9512a
Hariom Arya <105986024+Hariom0100@users.noreply.github.com>
2022-05-24 22:49:42 +0530

    Add files via upload
    https://eloquent-hamster-2c8f71.netlify.app

---
+++ Gatepass/logic.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', bindEvents);
+function bindEvents(){
+    var button = document.getElementById('cont7');
+button.addEventListener('click',f1);
+
+
+}
+
+function f1(){
+    document.getElementById('cont15').innerText="Sorry,It is under Maintenance!!!"
+}
